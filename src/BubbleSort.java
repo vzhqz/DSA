ca203e06e4aecd1a8db01b140c8a733d194c27c1
vzhqz <hagebd53@gmail.com>
2025-08-03 16:13:51 +0300

    added an explanation about the algorithm

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -7,11 +7,23 @@
 public class BubbleSort {
     private BubbleSort() { throw new UnsupportedOperationException(); }
 
+    /**
+     *
+     * The Bubble Sort Algorithm works by comparing adjacent pair of elements:
+     * <ul>
+     *     <li>If the pairs are in order, if not, the elements are swapped.</li>
+     *     <li>This goes on until all the elements are in order and the array is sorted.</li>
+     * </ul>
+     *
+     * <p>This sorting algorithm is fine for smaller data sets, but bad for large data sets.</p>
+     */
+
     // --- primitive data type (int) ---
     public static void sort(int[] array) {
         for(int i = 0; i < array.length - 1; i++) {
             for(int j = 0; j < array.length - i -1; j++) {
                 if(array[j] > array[j + 1]) {
+                    // swap the two indexes
                     int temp = array[j];
                     array[j] = array[j + 1];
                     array[j + 1] = temp;
